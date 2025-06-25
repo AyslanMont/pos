4baad40cd7f097991092bd0b2748adb8b97a9b9e
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2025-06-25 07:49:12 -0300

    Finalizando atividade de requisição da biblioteca

---
+++ atv_biblioteca_requets/require.py
@@ -0,0 +1,43 @@
+import requests
+
+if __name__ == "__main__":
+
+    url = "http://127.0.0.1:8000"
+
+    while True:
+
+        opcao = int(input("""
+        1 - Listar todos os livros
+        2 - Pesquisa livro por título
+        3 - Cadastrar um livro
+        4 - Deletar um livro
+        5 - Sair
+        """))
+
+        match opcao:
+            case 1:
+                response = requests.get(f"{url}/livros")
+                print(response.text)
+            case 2:
+                titulo = input("Digite o título do livro: ")
+                response = requests.get(f"{url}/livros/{titulo}")
+                print(response.text)
+            case 3:
+                titulo = input("Digite o título do livro: ")
+                ano = int(input("Digite o ano do livro: "))
+                edicao = int(input("Digite a edição do livro: "))
+
+                livro = {
+                    "titulo": titulo,
+                    "ano": ano,
+                    "edicao": edicao
+                }
+
+                response = requests.post(f"{url}/livros/", json=livro)
+                print(f"{response.text} --------- status:{response.status_code}")
+            case 4:
+                titulo = input("Digite o título do livro: ")
+                response = requests.delete(f"{url}/livros/{titulo}")
+                print(f"{response.text} --------- status:{response.status_code}")
+            case 5:
+                break
